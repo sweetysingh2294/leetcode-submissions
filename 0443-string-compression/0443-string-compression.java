7c6c11b4c0e43e76e13d45525603344ce482d0ab
Sweety Singh <105454392+sweetysingh2294@users.noreply.github.com>
2023-03-03 00:50:39 +0530

    Time: 1 ms (98.42%), Space: 42 MB (47.21%) - LeetHub

--- 0443-string-compression/0443-string-compression.java
+++ 0443-string-compression/0443-string-compression.java
@@ -1,6 +1,6 @@
 class Solution {
     public int compress(char[] chars) {
-         int indexAns = 0, index = 0;
+        int indexAns = 0, index = 0;
         while(index < chars.length){
             char currentChar = chars[index];
             int count = 0;
@@ -9,10 +9,16 @@ class Solution {
                 count++;
             }
             chars[indexAns++] = currentChar;
-            if(count != 1)
-                for(char c : Integer.toString(count).toCharArray()) 
-                    chars[indexAns++] = c;
+            if(count != 1){
+                 char[] s=Integer.toString(count).toCharArray();
+                 for(int i=0;i<s.length;i++){
+                 char c=s[i];
+                 chars[indexAns++]=c;
+                 }
+
+}
         }
         return indexAns;                
     }
 }
+            
