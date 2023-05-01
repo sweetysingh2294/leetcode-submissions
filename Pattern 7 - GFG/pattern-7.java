c111a96a92489984fa64edd086f56e3b72050c87
Sweety Singh <105454392+sweetysingh2294@users.noreply.github.com>
2023-05-01 19:25:11 +0530

    Added solution - LeetHub

--- Pattern 7 - GFG/pattern-7.java
+++ Pattern 7 - GFG/pattern-7.java
@@ -29,9 +29,9 @@ class Solution {
             for(int k=1;k<=2*i-1;k++){
                 System.out.print("*");
             }
-            for(int j=1;j<=space;j++){
-                System.out.print(" ");
-            }
+            // for(int j=1;j<=space;j++){
+            //     System.out.print(" ");
+            // }
             System.out.println();
         }
     }
