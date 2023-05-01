a6385b3c27ffc93220009adca9e5d1432f9a5a1b
Sweety Singh <105454392+sweetysingh2294@users.noreply.github.com>
2023-05-01 18:10:28 +0530

    Added solution - LeetHub

--- Pattern 3 - GFG/pattern-3.java
+++ Pattern 3 - GFG/pattern-3.java
@@ -22,8 +22,7 @@ class Solution {
 
     void printTriangle(int n) {
         for(int i=1;i<=n;i++){
-            int j=1;
-            for(j=1;j<=i;j++){
+            for(int j=1;j<=i;j++){
                 System.out.print(j+" ");
             }
             System.out.println();
